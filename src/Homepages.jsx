fffda3729f71f73ecaa2b6b8d84177349e584ae9
prettyangela <angelala6600@gmail.com>
2023-10-22 22:33:59 +0700

    update delete section main

--- src/Homepages.jsx
+++ src/Homepages.jsx
@@ -9,27 +9,13 @@ import Copyright from './Header/Copyright'
 function Homepages() {
     return(
         <>
-        <section id="navbar">
             <Navbar/>
-        </section>
-        <section id="home">
             <Home/>
-        </section>
-        <section id="layanan">
             <Layanan/>
-        </section>
-        <section id="artikel">
             <Artikel/>
-        </section>
-        <section id="form">
             <Form/>
-        </section>
-        <section id="footer">
             <Footer/>
-        </section>
-        <section id="copyright">
             <Copyright/>
-        </section>
         </>
     )
 }
